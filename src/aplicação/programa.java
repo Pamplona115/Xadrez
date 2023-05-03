326b0080f56f39c9dbb03a3dcdd7a900e94c5d74
Pamplona <jppamplona101@gmail.com>
2023-05-03 17:12:25 -0300

    classe: posição

---
+++ src/aplicação/programa.java
@@ -0,0 +1,14 @@
+package aplicação;
+
+import tabuleiro.posição;
+
+public class programa {
+
+	public static void main(String[] args) {
+		
+		//teste clase posição//
+		posição p = new posição(3, 5);
+		System.out.println(p);
+	}
+
+}
